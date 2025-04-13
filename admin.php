c39f22f241b80270e7cbc9864387a96e9a14b74d
Divina <christiandivina2316@gmail.com>
2025-04-13 23:36:36 +0800

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang='en'>
+<head>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+  <link rel="stylesheet" href="style.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <title>INVENTORY SYSTEM</title>
+</head>
+<body>
+<nav class="sidebar">
+  <header>
+    <div class="img-text">
+      <span class="img-text">
+        <img src="logo.png" alt="logo">
+      </span>
+      <div class="text header-text">
+        <span class="name">LELELEMON</span>
+        <span class="profession">INVENTORY</span>
+      </div>
+    </div>
+<i class='bx bx-chevron-right toggle'></i>
+</header>
+<ul class="menu-links">
+<li class="nav-link">
+    <a href="#">
+      <i class='bx bx-search icon'></i>
+      <input type="search" placeholder="search">
+    </a>
+  </li>
+  <li class="nav-link">
+    <a href="#">
+      <i class='bx bx-user'></i>
+      <span class="text nav-text">Profile</span>
+    </a>
+  </li>
+  <li class="nav-link">
+    <a href="#">
+      <i class='bx bxs-food-menu'></i>
+      <span class="text nav-text">Inventory</span>
+    </a>
+  </li>
+  <li class="nav-link">
+    <a href="#">
+      <i class='bx bx-signal-4'></i>
+      <span class="text nav-text">Dashboard</span>
+    </a>
+  </li>
+  <li class="nav-link">
+    <a href="#">
+    <i class='bx bx-log-out'></i>
+      <span class="text nav-text">Logout</span>
+    </a>
+  </li>
+</ul>
+</ul>
+    </ul>
+  </div>
+</div>
+</nav>
+
+</body>
+</html>
